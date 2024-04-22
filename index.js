ff5ace93068fd4123ff28063a0fec0bfe6fd4c7c
Minsoo Cho <textign@gmail.com>
2024-04-22 19:07:09 +0900

    add genres

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ async function fetchAPI() {
   return dataList;
 }
 
+async function genreApi() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTk5ZmRhOWE3MjY4ZTAwMTE3MTVhMjcxYjMzMTAwNSIsInN1YiI6IjY2MTdhMzllN2Q0MWFhMDE3ZDAwNzkxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.svBiDsUi6VuVUKtPq8DVrEd7G6rkepmITGwi_94lCk8",
+    },
+  };
+  const json = await fetch(
+    "https://api.themoviedb.org/3/genre/movie/list",
+    options
+  ).then((response) => response.json());
+  const genreList = json.genres;
+  return genreList;
+}
+
 function makeList(el) {
   const cardBox = document.getElementById("card-box");
   const list = document.createElement("li");
@@ -28,12 +45,13 @@ function makeList(el) {
      </div>
      <h3>${el.title}</h3>
      <p>${el.overview}</p>
-     <span>${el.vote_average.toFixed(2)}</span>
+     <span>⭐️ ${el.vote_average.toFixed(2)}</span>
     `;
 
   // 알러트
   list.addEventListener("click", function () {
     alert(`id: ${el.id}`);
+    detailModal(el);
   });
 }
 
@@ -77,6 +95,7 @@ function searchFn(event, dataList, cardBox) {
   });
 }
 
+// theme
 const btnToggle = document.getElementById("toggle");
 const wrapper = document.querySelector(".wrapper");
 btnToggle.addEventListener("click", function (event) {
@@ -88,3 +107,37 @@ btnToggle.addEventListener("click", function (event) {
     event.target.innerText = "🌞";
   }
 });
+
+// detail modal
+async function detailModal(el) {
+  const modal = wrapper.querySelector(".modal");
+  modal.style.display = "flex";
+  if (modal.classList.contains("modal")) {
+    modal.innerHTML = `
+    <div>
+      <div class="covered-img"></div>
+      <ul class="genre">
+      </ul>
+      <h3>${el.title}</h3>
+      <p>${el.overview}</p>
+    </div>
+    `;
+
+    const bgImg = wrapper.querySelector(".covered-img");
+    bgImg.style.backgroundImage = `url("https://image.tmdb.org/t/p/original/${el.backdrop_path}")`;
+
+    modal.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+
+    //장르
+    const genreList = await genreApi();
+    for (let searchId of el.genre_ids) {
+      const searchGenre = genreList.filter((item) => item.id === searchId);
+      searchGenre.map((genre) => {
+        const genreBox = wrapper.querySelector(".genre");
+        genreBox.innerHTML += `<li>${genre.name}</li>`;
+      });
+    }
+  }
+}
